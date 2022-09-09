886871821a41a39baa736b11539d0ae2b1ed4ca2
rodel.paco <rodel.paco@gmail.com>
2022-09-09 23:26:53 +0800

    Updated

---
+++ test.py
@@ -0,0 +1,21 @@
+from dotenv import load_dotenv
+load_dotenv()
+from binance import Client
+from pathlib import Path
+import requests
+import pandas as pd
+import ta
+import numpy as np
+import time
+import os
+
+api_key = os.getenv("api_key")
+api_secret = os.getenv("api_secret")
+api_telegram1 = os.getenv("api_telegram1")
+msg_id_telegram1 = os.getenv("msg_id_telegram1")
+file_path = os.getenv("file_path")
+
+client = Client(api_key, api_secret)
+
+client.API_URL = 'https://testnet.binance.vision/api'
+print(client.get_account())
