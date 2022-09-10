2ea2546a5118a1090ee2dc119d7ea008fc1efe03
rodel.paco <rodel.paco@gmail.com>
2022-09-10 15:55:00 +0800

    Updated

--- test.py
+++ test.py
@@ -85,7 +85,7 @@ def strategy(pair, qty, open_position=False):
         file.close()
         ##########################################################################################################
         if pair not in clean_buy_list:
-            order = client.futures_create_order(symbol=pair,side='BUY',type='MARKET',quantity=qty)
+            order = client.futures_create_order(symbol=pair,side='BUY',type='MARKET',quantity=qty,leverage=10)
             buyprice = order['fills'][0]['price']
             open_position = True
             #buyprice = str(df.Close.iloc[-1])
@@ -114,7 +114,7 @@ def strategy(pair, qty, open_position=False):
         file.close()
         ###########################################################################################################
         if pair not in clean_sell_list:
-            order = client.futures_create_order(symbol=pair,side='SELL',type='MARKET',quantity=qty)
+            order = client.futures_create_order(symbol=pair,side='SELL',type='MARKET',quantity=qty,leverage=10)
             body = pair, order, "SELL - 1 minute timeframe version. Current Price " + str(df.Close.iloc[-1])
             base_url = 'https://api.telegram.org/bot' + str(api_telegram1) + '/sendMessage?chat_id=' + str(msg_id_telegram1)+ '&text="{}"'.format(body)
             requests.get(base_url)
