e2cdaf5a8f18be22b37feb5d0047c930d088dde1
rodel.paco <rodel.paco@gmail.com>
2022-09-10 14:50:03 +0800

    Updated

--- test.py
+++ test.py
@@ -129,7 +129,7 @@ while True:
         myfile2 = Path(file_path+ coins +'_sell_1m.txt')
         myfile1.touch(exist_ok=True)
         myfile2.touch(exist_ok=True)
-        strategy(coins, 10.00000000)
+        strategy(coins, 5.0)
         time.sleep(10)
         # except Exception:
             # pass
