c417dbcf67faf4b825ad9266324c2a51eb1678ec
rodel.paco <rodel.paco@gmail.com>
2022-09-25 01:00:28 +0800

    Updated

--- print.py
+++ print.py
@@ -42,16 +42,15 @@ client = Client(api_key, api_secret)
 # #    if (int(float(item['price'])) < 1) and item['symbol'].endswith('BUSD'):
         # print(item)
 
-open_pos = client.futures_account()['positions']
-for item in open_pos:
-    if item['symbol'] == '1000SHIBBUSD':
-        print(item)
+# open_pos = client.futures_account()['positions']
+# for item in open_pos:
+    # if item['symbol'] == '1000SHIBBUSD':
+        # print(item)
 
 # open_pos1 = client.get_account()['balances']
 # for item in open_pos1:
     # if item['asset'] == 'SHIB':
         # print(item)
 
-# open_pos1 = client.get_lending_position()
-# for item in open_pos1:
-    # print(item)
+open_pos1 = client.account()
+    print(open_pos1)
