5f9b7c229689d407f41ed3426163734fbe7c8095
rodel.paco <rodel.paco@gmail.com>
2022-09-27 00:43:54 +0800

    Updated

--- crypto_bot_minute.py
+++ crypto_bot_minute.py
@@ -129,7 +129,7 @@ def strategy(pair, qty, open_position=False):
         file.close()
         ###########################################################################################################
         print(clean_buy_price_list,df.Close.iloc[-1])
-        if (pair not in clean_sell_list and (df.Close.iloc[-1] <= float(clean_buy_price_list) * float(.85))) or (pair not in clean_sell_list and df.Close.iloc[-1] > clean_buy_price_list):
+        if (pair not in clean_sell_list and (df.Close.iloc[-1] <= clean_buy_price_list * .85)) or (pair not in clean_sell_list and df.Close.iloc[-1] > clean_buy_price_list):
             body = pair,clean_buy_price_list,"SELL - 1 minute timeframe version. Current Price " + str(df.Close.iloc[-1])
             #sellprice = str(df.Close.iloc[-1])
             base_url = 'https://api.telegram.org/bot' + str(api_telegram1) + '/sendMessage?chat_id=' + str(msg_id_telegram1)+ '&text="{}"'.format(body)
