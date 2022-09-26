83000b0b2c5370347acab0b8e7abbd89dc5a48ca
rodel.paco <rodel.paco@gmail.com>
2022-09-26 22:34:21 +0800

    Updated

--- crypto_bot_minute.py
+++ crypto_bot_minute.py
@@ -115,7 +115,7 @@ def strategy(pair, qty, open_position=False):
         with open(file_path+ pair +'_buy_price_1m.txt', 'r') as f:
             clean_buy_price_list = []
             for buy_price_list in f.readlines():
-                if buy_price_list =< df.Close.iloc[-1]:
+                if buy_price_list < df.Close.iloc[-1]:
                     clean_buy_price_list.append(buy_price_list.replace("\n", ""))
                     file = open(file_path+ pair +'_buy_price_1m.txt', 'w')
                     file.close()
