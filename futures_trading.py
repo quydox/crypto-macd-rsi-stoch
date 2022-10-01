aff9e96b0026b0172bad04737a0ac4ddc379b1f1
rodel.paco <rodel.paco@gmail.com>
2022-10-01 10:15:45 +0800

    Updated

--- futures_trading.py
+++ futures_trading.py
@@ -127,7 +127,7 @@ while True:
     for coins in crypto_coins:
         try:
             current_price = client.get_symbol_ticker(symbol=coins)
-            total_coins = round(float(15/(float(current_price['price']))),3)
+            total_coins = round(float(150/(float(current_price['price']))),3)
             myfile1 = Path(file_path+ coins +'_buy_future.txt')
             myfile2 = Path(file_path+ coins +'_sell_future.txt')
             myfile1.touch(exist_ok=True)
