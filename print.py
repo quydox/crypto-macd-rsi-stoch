d17db14d4a433156dba61305319321ab3b515b60
rodel.paco <rodel.paco@gmail.com>
2022-10-02 16:33:59 +0800

    Updated

--- print.py
+++ print.py
@@ -1,3 +1,22 @@
+from dotenv import load_dotenv
+load_dotenv()
+from binance import Client
+from pathlib import Path
+import requests
+import pandas as pd
+import ta
+import numpy as np
+import time
+import os
+
+api_key = os.getenv("api_key")
+api_secret = os.getenv("api_secret")
+api_telegram1 = os.getenv("api_telegram1")
+msg_id_telegram1 = os.getenv("msg_id_telegram1")
+file_path = os.getenv("file_path")
+
+client = Client(api_key, api_secret)
+
 # crypto_coins = client.futures_symbol_ticker()
 # for item in crypto_coins:
 # #    if (int(float(item['price'])) < 1) and item['symbol'].endswith('BUSD'):
