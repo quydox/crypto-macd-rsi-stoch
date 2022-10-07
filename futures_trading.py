7bc2b7abfc1d4d0f66a68c1f21912e044af23e8c
rodel.paco <rodel.paco@gmail.com>
2022-10-08 02:14:11 +0800

    Updated

--- futures_trading.py
+++ futures_trading.py
@@ -67,7 +67,7 @@ def strategy(pair, qty, open_position=False):
     inst = Signals(df, 25)
     inst.decide()
     for open_position in active_position:
-        print(pair + f' Current Close is ' + str(df.Close.iloc[-1]) + f' Entry ' str(open_position['entryPrice']) +, str(df.macd.iloc[-1]), str(df.rsi.iloc[-1]) )
+        print(pair + f' Current Close is ' + str(df.Close.iloc[-1]) + f' Entry ' + str(open_position['entryPrice']), str(df.macd.iloc[-1]), str(df.rsi.iloc[-1]) )
         for check_balance in acc_balance:
             if check_balance['asset'] == "BUSD":
                 busd_balance = check_balance["balance"]
