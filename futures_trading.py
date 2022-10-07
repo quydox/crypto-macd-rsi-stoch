2e83a37153dd9686965650c32a0d9f0e29df0dec
rodel.paco <rodel.paco@gmail.com>
2022-10-08 02:58:37 +0800

    Updated

--- futures_trading.py
+++ futures_trading.py
@@ -115,7 +115,7 @@ def strategy(pair, qty, open_position=False):
                     file = open(file_path+ pair +'_buy_future.txt', 'w')
                     file.close()
                     ###########################################################################################################
-                    if pair not in clean_sell_list or int(float(df.Close.iloc[-1])) <= int(float(open_position['entryPrice'])) * 0.998:
+                    if pair not in clean_sell_list or int(float(df.Close.iloc[-1])) <= int(float(open_position['entryPrice'])) * 0.999:
                         fees = client.get_trade_fee(symbol=pair)
                         for item in fees:
                             qty_order = qty-(float(item['takerCommission'])*qty)
