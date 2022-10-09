b4a794a3e31e2607bfd7f7adc78f386ee7186b0b
rodel.paco <rodel.paco@gmail.com>
2022-10-09 15:23:45 +0800

    Updated

--- futures_trading_updated.py
+++ futures_trading_updated.py
@@ -54,8 +54,8 @@ class Signals:
 
     def decide(self):
         self.df['trigger'] = np.where(self.gettrigger(), 1, 0)
-        self.df['Buy'] = np.where((self.df.trigger) & (self.df['%K'].between(20,80)) & (self.df['%D'].between(20,80)) & (self.df.rsi > 50) & (self.df.macd > 0) & (self.df.ema > df.Close) , 1, 0)
-        self.df['Sell'] = np.where((self.df.trigger) & (self.df['%K'].between(20,80)) & (self.df['%D'].between(20,80)) & (self.df.rsi < 50) & (self.df.macd < 0) & (self.df.ema < df.Close), 1, 0)
+        self.df['Buy'] = np.where((self.df.trigger) & (self.df['%K'].between(20,30)) & (self.df['%D'].between(20,30)) & (self.df.rsi > 50) & (self.df.macd > 0) & (self.df.ema > df.Close) , 1, 0)
+        self.df['Sell'] = np.where((self.df.trigger) & (self.df['%K'].between(70,80)) & (self.df['%D'].between(70,80)) & (self.df.rsi < 50) & (self.df.macd < 0) & (self.df.ema < df.Close), 1, 0)
         # self.df['Buy'] = np.where((self.df.trigger) & (self.df['%K'].between(20,80)) & (self.df['%D'].between(20,80)) & (self.df.rsi > 50) & (self.df.macd > 0), 1, 0)
         # self.df['Sell'] = np.where((self.df.trigger) & (self.df['%K'].between(20,80)) & (self.df['%D'].between(20,80)) & (self.df.rsi < 50) & (self.df.macd < 0), 1, 0)
 
