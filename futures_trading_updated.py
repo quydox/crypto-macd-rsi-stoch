b627901813d9744982a5907c277be18cf4c5d9a3
rodel.paco <rodel.paco@gmail.com>
2022-10-09 16:50:39 +0800

    Updated

--- futures_trading_updated.py
+++ futures_trading_updated.py
@@ -26,7 +26,7 @@ def getminutedata(symbol, interval, lookback):
     frame = frame.astype(float)
     return frame
 
-df = getminutedata('BTCUSDT', '30m', "1 day ago UTC")
+df = getminutedata('BTCUSDT', '1h', "1 day ago UTC")
 # print(df)
 
 def applytechnicals(df):
