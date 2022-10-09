3966815d33e2b2b014fe1cb67d2c5c853a5c3d45
rodel.paco <rodel.paco@gmail.com>
2022-10-09 18:29:09 +0800

    Updated

--- futures_trading_updated.py
+++ futures_trading_updated.py
@@ -68,7 +68,7 @@ class Signals:
 inst = Signals(df, 25)
 inst.decide()
 # df[df.oversold == 1 ]
-print(df[df.Buy == 1])
+print(df[df.Sell == 1])
 
 # def strategy(pair, qty, open_position=False):
     # #df = getminutedata(pair, '15m', "1 day ago UTC")
