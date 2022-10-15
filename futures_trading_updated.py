1f6c7536db12695cdc75096803476d70578151e4
rodel.paco <rodel.paco@gmail.com>
2022-10-15 18:03:13 +0800

    Updated

--- futures_trading_updated.py
+++ futures_trading_updated.py
@@ -142,6 +142,6 @@ while True:
         myfile1.touch(exist_ok=True)
         myfile2.touch(exist_ok=True)
         strategy(coins, total_coins)
-        time.sleep(1)
+        time.sleep(5)
         # except Exception:
             # pass
