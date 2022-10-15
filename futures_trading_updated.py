d96d239cd8ced505baf9b3147167a5956ac1c660
rodel.paco <rodel.paco@gmail.com>
2022-10-15 17:55:11 +0800

    Updated

--- futures_trading_updated.py
+++ futures_trading_updated.py
@@ -71,7 +71,7 @@ def strategy(pair, qty, open_position=False):
         for check_balance in acc_balance:
             if check_balance['asset'] == "BUSD":
                 busd_balance = check_balance["balance"]
-                profit_balance = int(float(busd_balance))/90 * 100 - 100
+                profit_balance = int(float(busd_balance))/82 * 100 - 100
                 if df.Buy.iloc[-1]:
                     #####################Read the previous buy text output and empty the file ################################
                     with open(file_path+ pair +'_buy_future.txt', 'r') as f:
