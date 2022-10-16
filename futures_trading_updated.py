3791ca1b382f2abd812f83d8a4a8ae4da4b7dc59
rodel.paco <rodel.paco@gmail.com>
2022-10-16 09:50:19 +0800

    Updated

--- futures_trading_updated.py
+++ futures_trading_updated.py
@@ -89,7 +89,7 @@ def strategy(pair, qty, open_position=False):
                     file.close()
                     ##########################################################################################################
                     if pair not in clean_buy_list:
-                        order = client.futures_create_order(symbol=pair,side='BUY',type='MARKET',quantity=qty,leverage=20)
+                        order = client.futures_create_order(symbol=pair,side='BUY',type='MARKET',quantity=qty,leverage=50)
                         open_position = True
                         body = pair,"Profit: ",profit_balance, order, "BUY - 1m TF Close Price" + str(df.Close.iloc[-1]), " EMA " + str(df.ema.iloc[-1]), " MACD " + str(df.macd.iloc[-1])
                         base_url = 'https://api.telegram.org/bot' + str(api_telegram1) + '/sendMessage?chat_id=' + str(msg_id_telegram1)+ '&text="{}"'.format(body)
@@ -118,7 +118,7 @@ def strategy(pair, qty, open_position=False):
                         fees = client.get_trade_fee(symbol=pair)
                         for item in fees:
                             qty_order = qty-(float(item['takerCommission'])*qty)
-                            order = client.futures_create_order(symbol=pair,side='SELL',type='MARKET',quantity=qty_order,leverage=20)
+                            order = client.futures_create_order(symbol=pair,side='SELL',type='MARKET',quantity=qty_order,leverage=50)
                             body = pair,"Profit: ",profit_balance, order, "SELL - 1m TF. Close Price" + str(df.Close.iloc[-1]), " EMA " + str(df.ema.iloc[-1]), " MACD " + str(df.macd.iloc[-1])
                             base_url = 'https://api.telegram.org/bot' + str(api_telegram1) + '/sendMessage?chat_id=' + str(msg_id_telegram1)+ '&text="{}"'.format(body)
                             requests.get(base_url)
