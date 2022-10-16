7980b7c59b56cb9fb8cf1157b02067ae4478628c
Rodel Paco <rpaco@Rodels-MacBook-Pro.local>
2022-10-16 15:30:46 +0800

    Updated

--- futures_trading_updated.py
+++ futures_trading_updated.py
@@ -105,8 +105,6 @@ def strategy(pair, qty, open_position=False):
                         base_url = 'https://api.telegram.org/bot' + str(api_telegram1) + '/sendMessage?chat_id=' + str(msg_id_telegram1)+ '&text="{}"'.format(body)
                         requests.get(base_url)
                         print(body)
-                    with open(file_path+ pair +'_buy_future.txt', 'a+') as f:
-                        f.write(str(pair) + '\n')
                 elif df.Sell.iloc[-1]:
                     #####################Read the previous sell text output and empty the file ###############################
                     with open(file_path+ pair +'_sell_future.txt', 'r') as f:
