c6e52d58ec915608dc934ab93d5af9c927f32229
Rodel Paco <rpaco@Rodels-MacBook-Pro.local>
2022-10-18 00:36:41 +0800

    Updated

--- futures_trading_updated.py
+++ futures_trading_updated.py
@@ -58,12 +58,12 @@ class Signals:
         self.df['Sell'] = np.where((self.df.trigger) & (self.df['%K'].between(20,80)) & (self.df['%D'].between(20,80)) & (self.df.macd < 0) & (self.df.ema < df.Close), 1, 0)
         self.df['Sell1'] = np.where((self.df.trigger) & (self.df['%K'].between(20,80)) & (self.df['%D'].between(20,80)) & (self.df.rsi < 50) & (self.df.macd < 0) & (self.df.ema > self.df.Close), 1, 0)
         self.df['Test'] = np.where((self.df.trigger) & (self.df['%K'].between(20,80)), 1, 0)
-        self.df['rsiSELL'] = np.where((self.df.trigger) & (self.df.rsi < 50), 1, 0)
         self.df['rsiBUY'] = np.where((self.df.trigger) & (self.df.rsi > 50), 1, 0)
-        self.df['emaSELL'] = np.where((self.df.trigger) & (self.df.ema > self.df.Close), 1, 0)
+        self.df['macdBUY'] = np.where((self.df.trigger) & (self.df.macd > 0), 1, 0)
         self.df['emaBUY'] = np.where((self.df.trigger) & (self.df.ema < self.df.Close), 1, 0)
         self.df['macdSELL'] = np.where((self.df.trigger) & (self.df.macd < 0), 1, 0)
-        self.df['macdBUY'] = np.where((self.df.trigger) & (self.df.macd > 0), 1, 0)
+        self.df['rsiSELL'] = np.where((self.df.trigger) & (self.df.rsi < 50), 1, 0)
+        self.df['emaSELL'] = np.where((self.df.trigger) & (self.df.ema > self.df.Close), 1, 0)
 
 
 # inst = Signals(df, 2)
