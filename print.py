948c1590dfe7c8b05b993c90d46b9e6ca6f1d115
Rodel Paco <rpaco@Rodels-MacBook-Pro.local>
2022-10-18 11:41:12 +0800

    Updated

--- print.py
+++ print.py
@@ -125,7 +125,7 @@ class Signals:
         return dfx.sum(axis=0)
 
     def decide(self):
-        self.df['trigger'] = np.where(self.gettrigger())
+        self.df['trigger'] = np.where(self.gettrigger(), 1, 0)
 #         self.df['Buy'] = np.where((self.df.trigger) & (self.df['%K'].between(20,80)) & (self.df['%D'].between(20,80)) & (self.df.rsi > 50) & (self.df.macd > 0) & (self.df.ema < self.df.Close), 1, 0)
 #         self.df['Sell'] = np.where((self.df.trigger) & (self.df['%K'].between(20,80)) & (self.df['%D'].between(20,80)) & (self.df.rsi < 50) & (self.df.macd < 0) & (self.df.ema > self.df.Close), 1, 0)
 #         self.df['Stochastic'] = np.where((self.df.trigger) & (self.df['%K'].between(20,80)), 1, 0)
