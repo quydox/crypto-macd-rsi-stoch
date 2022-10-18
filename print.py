5029537583239f7ac9508a0791d982216b1b4982
Rodel Paco <rpaco@Rodels-MacBook-Pro.local>
2022-10-18 11:34:42 +0800

    Updated

--- print.py
+++ print.py
@@ -67,13 +67,13 @@ client = Client(api_key, api_secret)
     # if item['symbol'] == "BTCBUSD":
         # print(item)
 
-active_position = client.futures_position_information(symbol='BTCBUSD')
-for open_position_check in active_position:
-    print(str(open_position_check['entryPrice']))
-    if float(open_position_check['entryPrice']) != 0:
-        print("Entry price not equal zero")
-    if float(open_position_check['entryPrice']) == 0:
-        print("Entry price is equal zero")
+# active_position = client.futures_position_information(symbol='BTCBUSD')
+# for open_position_check in active_position:
+#     print(str(open_position_check['entryPrice']))
+#     if float(open_position_check['entryPrice']) != 0:
+#         print("Entry price not equal zero")
+#     if float(open_position_check['entryPrice']) == 0:
+#         print("Entry price is equal zero")
     # #if int(float(df.Close.iloc[-1])) > int(float(item['entryPrice'])):
     # if close_price > int(float(open_position_check['entryPrice'])):
     #     print("BUY", str(open_position_check['entryPrice']))
@@ -88,3 +88,55 @@ for open_position_check in active_position:
 
 # fees = client.get_trade_fee(symbol='BTCBUSD')
 # print(fees)
+
+def getminutedata(symbol, interval, lookback):
+    frame = pd.DataFrame(client.futures_historical_klines(symbol, interval, lookback))
+    frame = frame.iloc[:,:6]
+    frame.columns = ['Time', 'Open', 'High', 'Low', 'Close', 'Volume']
+    frame = frame.set_index('Time')
+    frame.index = pd.to_datetime(frame.index, unit='ms')
+    frame = frame.astype(float)
+    return frame
+
+df = getminutedata('BTCUSDT', '1m', "1 day ago SGT")
+# print(df)
+
+def applytechnicals(df):
+    df['%K'] = ta.momentum.stoch(df.High,df.Low,df.Close, window=5, smooth_window=3)
+    df['%D'] = df['%K'].rolling(3).mean()
+    df['rsi'] = ta.momentum.rsi(df.Close, window=14)
+    df['macd'] = ta.trend.macd_diff(df.Close, window_slow=21, window_fast=8, window_sign=5)
+    df['ema'] = ta.trend.ema_indicator(df.Close, window=9)
+    df.dropna(inplace=True)
+
+applytechnicals(df)
+print(df)
+
+# class Signals:
+#     def __init__(self,df, lags):
+#         self.df = df
+#         self.lags = lags
+
+#     def gettrigger(self):
+#         dfx = pd.DataFrame()
+#         for i in range(self.lags +1):
+#             mask = (self.df['%K'].shift(i) < 20) & (self.df['%D'].shift(i) < 20)
+#             dfx = pd.concat([mask], ignore_index=True)
+#         return dfx.sum(axis=0)
+
+#     def decide(self):
+#         self.df['trigger'] = np.where(self.gettrigger(), 1, 0)
+#         self.df['Buy'] = np.where((self.df.trigger) & (self.df['%K'].between(20,80)) & (self.df['%D'].between(20,80)) & (self.df.rsi > 50) & (self.df.macd > 0) & (self.df.ema < self.df.Close), 1, 0)
+#         self.df['Sell'] = np.where((self.df.trigger) & (self.df['%K'].between(20,80)) & (self.df['%D'].between(20,80)) & (self.df.rsi < 50) & (self.df.macd < 0) & (self.df.ema > self.df.Close), 1, 0)
+#         self.df['Stochastic'] = np.where((self.df.trigger) & (self.df['%K'].between(20,80)), 1, 0)
+#         self.df['rsiBUY'] = np.where((self.df.trigger) & (self.df.rsi > 50), 1, 0)
+#         self.df['macdBUY'] = np.where((self.df.trigger) & (self.df.macd > 0), 1, 0)
+#         self.df['emaBUY'] = np.where((self.df.trigger) & (self.df.ema < self.df.Close), 1, 0)
+#         self.df['macdSELL'] = np.where((self.df.trigger) & (self.df.macd < 0), 1, 0)
+#         self.df['rsiSELL'] = np.where((self.df.trigger) & (self.df.rsi < 50), 1, 0)
+#         self.df['emaSELL'] = np.where((self.df.trigger) & (self.df.ema > self.df.Close), 1, 0)
+
+
+# inst = Signals(df, 2)
+# inst.decide()
+# print(df)
