3cc83e2b63f607148d3f2f2ba72359f2b55d3fd4
Rodel Paco <rpaco@Rodels-MacBook-Pro.local>
2022-10-18 13:37:11 +0800

    Updated

--- futures_trading_updated.py
+++ futures_trading_updated.py
@@ -106,7 +106,7 @@ def strategy(pair, qty, open_position=False):
                     file.close()
                     ##########################################################################################################
                     if pair not in clean_buy_list:
-                        order = client.futures_create_order(symbol=pair, side='BUY', type='MARKET', quantity=qty, leverage=30)
+                        order = client.futures_create_order(symbol=pair, side='BUY', type='MARKET', quantity=qty, leverage=50)
                         open_position = True
                         body = pair, "\n" + "PROFIT: ", profit_balance, "\n" + "ORDER: ", order,"\n" + "BUY - TAKE PROFIT FROM SELL: ", str(df.Close.iloc[-1]), "\n" + "EMA: ", str(df.ema.iloc[-1]), "\n" + " MACD: ", str(df.macd.iloc[-1])
                         #body = "BUY - TAKE PROFIT FROM SELL"
@@ -117,7 +117,7 @@ def strategy(pair, qty, open_position=False):
                         f.write(str(pair) + '\n')
                     ##########################################################################################################
                     if pair in clean_buy_list and float(open_position_check['entryPrice']) == 0:
-                        order = client.futures_create_order(symbol=pair, side='BUY', type='MARKET', quantity=qty, leverage=30)
+                        order = client.futures_create_order(symbol=pair, side='BUY', type='MARKET', quantity=qty, leverage=50)
                         ##stoploss_buy = client.futures_create_order(symbol=pair, side='BUY', type='STOP_MARKET', stopPrice=int(stop_loss_market_buy), closePosition='true')
                         open_position = True
                         body = pair, "\n" + "PROFIT: ", profit_balance, "\n" + "ORDER: ", order, "\n" + "BUY - NEW ENTRY: ", str(df.Close.iloc[-1]), "\n" + "EMA: ", str(df.ema.iloc[-1]), "\n" + " MACD: ", str(df.macd.iloc[-1])
@@ -148,7 +148,7 @@ def strategy(pair, qty, open_position=False):
                         fees = client.get_trade_fee(symbol=pair)
                         for item in fees:
                             qty_order = qty-(float(item['takerCommission'])*qty)
-                            order = client.futures_create_order(symbol=pair, side='SELL', type='MARKET', quantity=qty_order, leverage=30)
+                            order = client.futures_create_order(symbol=pair, side='SELL', type='MARKET', quantity=qty_order, leverage=50)
                             body = pair, "\n" + "PROFIT: ", profit_balance, "\n" + "ORDER: ", order,"\n" + "SELL - TAKE PROFIT FROM BUY: ", str(df.Close.iloc[-1]), "\n" + "EMA: ", str(df.ema.iloc[-1]), "\n" + " MACD: ", str(df.macd.iloc[-1])
                             #body = "SELL - TAKE PROFIT FROM BUY"
                             base_url = 'https://api.telegram.org/bot' + str(api_telegram1) + '/sendMessage?chat_id=' + str(msg_id_telegram1)+ '&text="{}"'.format(body)
@@ -161,7 +161,7 @@ def strategy(pair, qty, open_position=False):
                         fees = client.get_trade_fee(symbol=pair)
                         for item in fees:
                             qty_order = qty-(float(item['takerCommission'])*qty)
-                            order = client.futures_create_order(symbol=pair, side='SELL', type='MARKET', quantity=qty_order, leverage=30)
+                            order = client.futures_create_order(symbol=pair, side='SELL', type='MARKET', quantity=qty_order, leverage=50)
                             body = pair, "\n" + "PROFIT: ", profit_balance, "\n" + "ORDER: ", order,"\n" + "SELL - TAKE PROFIT FROM BUY: ", str(df.Close.iloc[-1]), "\n" + "EMA: ", str(df.ema.iloc[-1]), "\n" + " MACD: ", str(df.macd.iloc[-1])
                             #body = "SELL - TAKE PROFIT FROM BUY"
                             base_url = 'https://api.telegram.org/bot' + str(api_telegram1) + '/sendMessage?chat_id=' + str(msg_id_telegram1)+ '&text="{}"'.format(body)
