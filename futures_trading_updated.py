572314201947f20ccb517bda012711673e7cdaff
Rodel Paco <rpaco@Rodels-MacBook-Pro.local>
2022-10-20 18:48:07 +0800

    Updated

--- futures_trading_updated.py
+++ futures_trading_updated.py
@@ -171,7 +171,7 @@ while True:
         current_price = client.get_symbol_ticker(symbol=coins)
         stop_loss_market_buy = int(float(current_price['price']) * 0.995)
         stop_loss_market_sell = int(float(current_price['price']) * 1.005)
-        total_coins = round(float(60/(float(current_price['price']))),3)
+        total_coins = round(float(280/(float(current_price['price']))),3)
         myfile1 = Path(file_path+ coins +'_buy_future.txt')
         myfile2 = Path(file_path+ coins +'_sell_future.txt')
         myfile1.touch(exist_ok=True)
