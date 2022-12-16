44d56f524129c784cbe54f33cb5c0a1a326b2b09
Rodel Paco <rpaco@Rodels-MacBook-Pro.local>
2022-12-16 22:51:26 +0800

    Updated

--- futures_trading_ema_alert.py
+++ futures_trading_ema_alert.py
@@ -178,7 +178,7 @@ while True:
     crypto_coins = ["BTCUSDT"]
     for coins in crypto_coins:
         # try:
-        df = getminutedata(coins, '1h', "30 days ago SGT")
+        df = getminutedata(coins, '5m', "1 day ago SGT")
         myfile1 = Path(file_path+ coins +'_buy_future_ema_alert.txt')
         myfile2 = Path(file_path+ coins +'_sell_future_ema_alert.txt')
         myfile1.touch(exist_ok=True)
