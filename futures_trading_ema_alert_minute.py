6aa1ab3f5dc8fe175398900a12cb1e53c1e59117
rodel.paco <rodel.paco@gmail.com>
2022-12-27 02:41:06 +0800

    Updated

--- futures_trading_ema_alert_minute.py
+++ futures_trading_ema_alert_minute.py
@@ -74,7 +74,7 @@ class Signals:
         self.df['TPBUY2'] = np.where((self.df.trigger) & (self.df.rsi > 70) & (self.df['%K'] < self.df['%D']), 1, 0)
         self.df['TPSELL2'] = np.where((self.df.trigger) & (self.df.rsi < 30) & (self.df['%K'] > self.df['%D']), 1, 0)
         self.df['TPBUY3'] = np.where((self.df.trigger) & (self.df.macd < 0) & (self.df.ema50 < self.df.ema100), 1, 0)
-        self.df['TPSELL3'] = np.where((self.df.trigger) & ((self.df.macd > 0) & (self.df.ema50 > self.df.ema100)) or ((self.df.rsi < 30) & (self.df['%K'] > self.df['%D'])), 1, 0)
+        self.df['TPSELL3'] = np.where((self.df.trigger) & (self.df.macd > 0) & (self.df.ema50 > self.df.ema100), 1, 0)
         self.df['uptrend'] = np.where((self.df.trigger) & (self.df.ema50 > self.df.ema150) & (self.df.ema100 > self.df.ema150), 1, 0)
         self.df['downtrend'] = np.where((self.df.trigger) & (self.df.ema50 < self.df.ema150) & (self.df.ema100 < self.df.ema150), 1, 0)
 
