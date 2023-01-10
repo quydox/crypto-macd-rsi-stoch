df777e8566a4b7f65ca151359cfefff8c3e7b31f
rodel.paco <rodel.paco@gmail.com>
2023-01-10 21:18:21 +0800

    Updated

--- crypto_bot.py
+++ crypto_bot.py
@@ -26,104 +26,104 @@ def getminutedata(symbol, interval, lookback):
     frame = frame.astype(float)
     return frame
 
-#df = getminutedata('BTCUSDT', '4h', "30 day ago UTC")
-#print(df)
+df = getminutedata('BTCUSDT', '4h', "30 day ago UTC")
+print(df)
 
-def applytechnicals(df):
-    df['%K'] = ta.momentum.stoch(df.High,df.Low,df.Close, window=14, smooth_window=3)
-    df['%D'] = df['%K'].rolling(3).mean()
-    df['rsi'] = ta.momentum.rsi(df.Close, window=14)
-    df['macd'] = ta.trend.macd_diff(df.Close)
-    df.dropna(inplace=True)
+# def applytechnicals(df):
+    # df['%K'] = ta.momentum.stoch(df.High,df.Low,df.Close, window=14, smooth_window=3)
+    # df['%D'] = df['%K'].rolling(3).mean()
+    # df['rsi'] = ta.momentum.rsi(df.Close, window=14)
+    # df['macd'] = ta.trend.macd_diff(df.Close)
+    # df.dropna(inplace=True)
 
-#applytechnicals(df)
-#print(df)
+# #applytechnicals(df)
+# #print(df)
 
-class Signals:
-    def __init__(self,df, lags):
-        self.df = df
-        self.lags = lags
+# class Signals:
+    # def __init__(self,df, lags):
+        # self.df = df
+        # self.lags = lags
 
-    def gettrigger(self):
-        dfx = pd.DataFrame()
-        for i in range(self.lags +1):
-            mask = (self.df['%K'].shift(i) < 20) & (self.df['%D'].shift(i) < 20)
-            dfx = pd.concat([mask], ignore_index=True)
-        return dfx.sum(axis=0)
+    # def gettrigger(self):
+        # dfx = pd.DataFrame()
+        # for i in range(self.lags +1):
+            # mask = (self.df['%K'].shift(i) < 20) & (self.df['%D'].shift(i) < 20)
+            # dfx = pd.concat([mask], ignore_index=True)
+        # return dfx.sum(axis=0)
 
-    def decide(self):
-        self.df['trigger'] = np.where(self.gettrigger(), 1, 0)
-        self.df['Buy'] = np.where((self.df.trigger) & (self.df['%K'].between(20,80)) & (self.df['%D'].between(20,80)) & (self.df.rsi > 50) & (self.df.macd > 0), 1, 0)
-        self.df['Sell'] = np.where((self.df.trigger) & (self.df['%K'].between(20,80)) & (self.df['%D'].between(20,80)) & (self.df.rsi < 50) & (self.df.macd < 0), 1, 0)
+    # def decide(self):
+        # self.df['trigger'] = np.where(self.gettrigger(), 1, 0)
+        # self.df['Buy'] = np.where((self.df.trigger) & (self.df['%K'].between(20,80)) & (self.df['%D'].between(20,80)) & (self.df.rsi > 50) & (self.df.macd > 0), 1, 0)
+        # self.df['Sell'] = np.where((self.df.trigger) & (self.df['%K'].between(20,80)) & (self.df['%D'].between(20,80)) & (self.df.rsi < 50) & (self.df.macd < 0), 1, 0)
 
-#inst = Signals(df, 25)
-#inst.decide()
-#df[df.Buy == 1 ]
-#print(df)
+# #inst = Signals(df, 25)
+# #inst.decide()
+# #df[df.Buy == 1 ]
+# #print(df)
 
-def strategy(pair, qty, open_position=False):
-    df = getminutedata(pair, '4h', "30 day ago UTC")
-    applytechnicals(df)
-    inst = Signals(df, 25)
-    inst.decide()
-    print(pair + f' Current Close is ' + str(df.Close.iloc[-1]), str(df.macd.iloc[-1]), str(df.rsi.iloc[-1]))
-    if df.Buy.iloc[-1]:
-        #####################Read the previous buy text output and empty the file ################################
-        with open('/root/trading/'+ pair +'_buy.txt', 'r') as f:
-            clean_buy_list = []
-            for buy_list in f.readlines():
-                clean_buy_list.append(buy_list.replace("\n", ""))
-        file = open('/root/trading/'+ pair +'_buy.txt', 'w')
-        file.close()
-        ###########################################################################################################
-        #####################Read the previous sell text output and empty the file ###############################
-        with open('/root/trading/'+ pair +'_sell.txt', 'r') as f:
-            clean_sell_list = []
-            for sell_list in f.readlines():
-                clean_sell_list.append(sell_list.replace("\n", ""))
-        file = open('/root/trading/'+ pair +'_sell.txt', 'w')
-        file.close()
-        ##########################################################################################################
-        if pair not in clean_buy_list:
-            buyprice = str(df.Close.iloc[-1])
-            body = pair,"BUY - 4 hour timeframe " + str(df.Close.iloc[-1])
-            base_url = 'https://api.telegram.org/bot' + str(api_telegram) + '/sendMessage?chat_id=' + str(msg_id_telegram)+ '&text="{}"'.format(body)
-            requests.get(base_url)
-            print(body)
-        with open('/root/trading/'+ pair +'_buy.txt', 'a+') as f:
-            f.write(str(pair) + '\n')
-    elif df.Sell.iloc[-1]:
-        #####################Read the previous sell text output and empty the file ###############################
-        with open('/root/trading/'+ pair +'_sell.txt', 'r') as f:
-            clean_sell_list = []
-            for sell_list in f.readlines():
-                clean_sell_list.append(sell_list.replace("\n", ""))
-        file = open('/root/trading/'+ pair +'_sell.txt', 'w')
-        file.close()
-        ##########################################################################################################
-        #####################Read the previous buy text output and empty the file ################################
-        with open('/root/trading/'+ pair +'_buy.txt', 'r') as f:
-            clean_buy_list = []
-            for buy_list in f.readlines():
-                clean_buy_list.append(buy_list.replace("\n", ""))
-        file = open('/root/trading/'+ pair +'_buy.txt', 'w')
-        file.close()
-        ###########################################################################################################
-        if pair not in clean_sell_list:
-            body = pair,"SELL - 4 hour timeframe " + str(df.Close.iloc[-1])
-            base_url = 'https://api.telegram.org/bot' + str(api_telegram) + '/sendMessage?chat_id=' + str(msg_id_telegram)+ '&text="{}"'.format(body)
-            requests.get(base_url)
-            print(body)
-        with open('/root/trading/'+ pair +'_sell.txt', 'a+') as f:
-            f.write(str(pair) + '\n')
-while True:
-    crypto_coins = ["BTCUSDT", "SLPUSDT", "AXSUSDT", "ETHUSDT", "SHIBUSDT"]
-    for coins in crypto_coins:
-        myfile1 = Path(file_path+ coins+'_buy.txt')
-        myfile2 = Path(file_path+ coins +'_sell.txt')
-        myfile1.touch(exist_ok=True)
-        myfile2.touch(exist_ok=True)
-        strategy(coins, 50)
-        time.sleep(60)
+# def strategy(pair, qty, open_position=False):
+    # df = getminutedata(pair, '4h', "30 day ago UTC")
+    # applytechnicals(df)
+    # inst = Signals(df, 25)
+    # inst.decide()
+    # print(pair + f' Current Close is ' + str(df.Close.iloc[-1]), str(df.macd.iloc[-1]), str(df.rsi.iloc[-1]))
+    # if df.Buy.iloc[-1]:
+        # #####################Read the previous buy text output and empty the file ################################
+        # with open('/root/trading/'+ pair +'_buy.txt', 'r') as f:
+            # clean_buy_list = []
+            # for buy_list in f.readlines():
+                # clean_buy_list.append(buy_list.replace("\n", ""))
+        # file = open('/root/trading/'+ pair +'_buy.txt', 'w')
+        # file.close()
+        # ###########################################################################################################
+        # #####################Read the previous sell text output and empty the file ###############################
+        # with open('/root/trading/'+ pair +'_sell.txt', 'r') as f:
+            # clean_sell_list = []
+            # for sell_list in f.readlines():
+                # clean_sell_list.append(sell_list.replace("\n", ""))
+        # file = open('/root/trading/'+ pair +'_sell.txt', 'w')
+        # file.close()
+        # ##########################################################################################################
+        # if pair not in clean_buy_list:
+            # buyprice = str(df.Close.iloc[-1])
+            # body = pair,"BUY - 4 hour timeframe " + str(df.Close.iloc[-1])
+            # base_url = 'https://api.telegram.org/bot' + str(api_telegram) + '/sendMessage?chat_id=' + str(msg_id_telegram)+ '&text="{}"'.format(body)
+            # requests.get(base_url)
+            # print(body)
+        # with open('/root/trading/'+ pair +'_buy.txt', 'a+') as f:
+            # f.write(str(pair) + '\n')
+    # elif df.Sell.iloc[-1]:
+        # #####################Read the previous sell text output and empty the file ###############################
+        # with open('/root/trading/'+ pair +'_sell.txt', 'r') as f:
+            # clean_sell_list = []
+            # for sell_list in f.readlines():
+                # clean_sell_list.append(sell_list.replace("\n", ""))
+        # file = open('/root/trading/'+ pair +'_sell.txt', 'w')
+        # file.close()
+        # ##########################################################################################################
+        # #####################Read the previous buy text output and empty the file ################################
+        # with open('/root/trading/'+ pair +'_buy.txt', 'r') as f:
+            # clean_buy_list = []
+            # for buy_list in f.readlines():
+                # clean_buy_list.append(buy_list.replace("\n", ""))
+        # file = open('/root/trading/'+ pair +'_buy.txt', 'w')
+        # file.close()
+        # ###########################################################################################################
+        # if pair not in clean_sell_list:
+            # body = pair,"SELL - 4 hour timeframe " + str(df.Close.iloc[-1])
+            # base_url = 'https://api.telegram.org/bot' + str(api_telegram) + '/sendMessage?chat_id=' + str(msg_id_telegram)+ '&text="{}"'.format(body)
+            # requests.get(base_url)
+            # print(body)
+        # with open('/root/trading/'+ pair +'_sell.txt', 'a+') as f:
+            # f.write(str(pair) + '\n')
+# while True:
+    # crypto_coins = ["BTCUSDT", "SLPUSDT", "AXSUSDT", "ETHUSDT", "SHIBUSDT"]
+    # for coins in crypto_coins:
+        # myfile1 = Path(file_path+ coins+'_buy.txt')
+        # myfile2 = Path(file_path+ coins +'_sell.txt')
+        # myfile1.touch(exist_ok=True)
+        # myfile2.touch(exist_ok=True)
+        # strategy(coins, 50)
+        # time.sleep(60)
 
-#Test
+# #Test
