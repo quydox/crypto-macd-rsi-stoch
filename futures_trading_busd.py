b2dfdc67a9927bdb2f4b38d7b2a0573adc352ad8
rodel.paco <rodel.paco@gmail.com>
2023-02-05 22:53:39 +0800

    Updated

--- futures_trading_busd.py
+++ futures_trading_busd.py
@@ -62,8 +62,6 @@ class Signals:
         self.df['Sell'] = np.where((self.df.trigger) & (self.df['%K'].between(20,80)) & (self.df['%D'].between(20,80)) & (self.df.ema5 < self.df.ema8) & (self.df.ema5 < self.df.ema10) & (self.df.ema8 < self.df.ema10) & (self.df['rsi'].between(44,50)), 1, 0)
         self.df['TPBUY1'] = np.where((self.df.trigger) & (self.df.ema5 < self.df.ema8) & (self.df.uptrend.iloc[-1]), 1, 0)
         self.df['TPSELL1'] = np.where((self.df.trigger) & (self.df.ema5 > self.df.ema8) & (self.df.downtrend.iloc[-1]), 1, 0)
-        self.df['TPBUY2'] = np.where((self.df.trigger) & (self.df.downtrend.iloc[-1]), 1, 0)
-        self.df['TPSELL2'] = np.where((self.df.trigger) & (self.df.uptrend.iloc[-1]), 1, 0)
 # inst = Signals(df, 2)
 # inst.decide()
 # print(df)
@@ -88,7 +86,7 @@ def strategy(pair, qty, open_position=False):
                     base_url = 'https://api.telegram.org/bot' + str(api_telegram1) + '/sendMessage?chat_id=' + str(msg_id_telegram1) + '&text="{}"'.format(body)
                     requests.get(base_url)
                     print(body)
-                elif ((df.TPBUY1.iloc[-1] or df.TPBUY2.iloc[-1]) and float(open_position_check['entryPrice']) != 0):
+                elif (df.TPBUY1.iloc[-1] and float(open_position_check['entryPrice']) != 0):
                     fees = client.get_trade_fee(symbol=pair)
                     for item in fees:
                         qty_order = qty-(float(item['takerCommission'])*qty)
@@ -108,7 +106,7 @@ def strategy(pair, qty, open_position=False):
                         base_url = 'https://api.telegram.org/bot' + str(api_telegram1) + '/sendMessage?chat_id=' + str(msg_id_telegram1)+ '&text="{}"'.format(body)
                         requests.get(base_url)
                         print(body)
-                elif ((df.TPSELL1.iloc[-1] or df.TPSELL2.iloc[-1]) and float(open_position_check['entryPrice']) != 0):
+                elif (df.TPSELL1.iloc[-1] and float(open_position_check['entryPrice']) != 0):
                     order = client.futures_create_order(symbol=pair, side='BUY', type='MARKET', quantity=qty, leverage=2)
                     open_position = True
                     body = "TAKE PROFIT FROM SELL: " + pair + "\n" + "CLOSE PRICE: " + str(df.Close.iloc[-1]) + "\n" + "ENTRY PRICE: " + str(open_position_check['entryPrice']) + "\n" + "TP1: " + str(df.TPSELL1.iloc[-1])
@@ -123,8 +121,8 @@ while True:
         acc_balance = client.futures_account_balance()
         active_position = client.futures_position_information(symbol=coins)
         current_price = client.get_symbol_ticker(symbol=coins)
-        stop_loss_market_buy = int(float(current_price['price']) * 0.998)
-        stop_loss_market_sell = int(float(current_price['price']) * 1.002)
+        stop_loss_market_buy = int(float(current_price['price']) * 0.999)
+        stop_loss_market_sell = int(float(current_price['price']) * 1.001)
         total_coins = round(float(50/(float(current_price['price']))),3)
         strategy(coins, total_coins)
         time.sleep(5)
