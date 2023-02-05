3c9fa7e5cc33678a272a7b5012fd173e6357574f
rodel.paco <rodel.paco@gmail.com>
2023-02-05 15:32:57 +0800

    Updated

--- print.py
+++ print.py
@@ -16,7 +16,7 @@ msg_id_telegram1 = os.getenv("msg_id_telegram1")
 file_path = os.getenv("file_path")
 
 client = Client(api_key, api_secret)
-
+pair = "BTCBUSD"
 file = open(file_path+ pair +'test.txt', 'w')
 file.close()
 
